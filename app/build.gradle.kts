4c6b8e10d64390797942487405c8779eff904f9e
kyrina-26 <kyrina.mollison@utronto.ca>
2025-11-17 18:19:31 -0500

    fix: Fix Google Services JSON and gradle.kts. Removed old package files causing conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,7 @@ plugins {
     alias(libs.plugins.google.gms.google.services)
 }
 
-android {
-    namespace = "com.SmartAir"
+android {namespace = "com.SmartAir"
     compileSdk = 34
 
     defaultConfig {
@@ -20,22 +19,21 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-}
+} // <-- This closing brace was missing
 
 dependencies {
-    // Import the Firebase BoM, this manages the versions of the other Firebase libraries
-    //this is needed to maintain the compatibility of the firebase libraries
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-
-    // Add the dependencies for the Firebase products you want to use
-    //In this case we can use the built in auth and db libraries
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-database")
 
@@ -43,6 +41,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
