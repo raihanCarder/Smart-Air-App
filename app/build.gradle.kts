150a4bc8437083855b13aaaf15e8d7ae385a2123
kyrina-26 <kyrina.mollison@utronto.ca>
2025-11-26 04:04:36 -0500

    chore: remove unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
 }
 
 dependencies {
@@ -40,16 +44,18 @@ dependencies {
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-appcheck-debug")
 
-    // This is the missing dependency that fixes the errors
-    implementation("com.google.firebase:firebase-functions")
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.firestore)
 
+    // JUnit and Mockito for Local Unit Tests
     testImplementation(libs.junit)
+    testImplementation("org.mockito:mockito-core:4.5.1")
+    testImplementation("org.mockito:mockito-inline:4.5.1")
+
+
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
