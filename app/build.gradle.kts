3f3ac24825f8830d36765d836f4e454b1f9e90c4
arshdippamma <187570030+arshdippamma@users.noreply.github.com>
2025-11-28 04:23:54 -0500

    feat(child-dashboard): implemented badges page with access from child dashboard.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.firestore)
+    implementation(libs.cardview)
 
     implementation(libs.firebase.database)
     implementation(libs.firebase.firestore)
