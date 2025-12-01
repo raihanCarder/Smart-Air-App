69bdd591f7160b22a0d812f96a2d91672b5f3260
Raihan C <139701695+raihanCarder@users.noreply.github.com>
2025-12-01 01:04:56 -0500

    feat: fix API 26 errors by enabling Java 8 desugaring support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
     }
 
     testOptions {
@@ -38,6 +39,8 @@ android {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-database")
